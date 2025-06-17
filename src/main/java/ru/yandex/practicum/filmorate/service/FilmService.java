49831bbc7e8f542507849173d5189d917d33d454
Anatoliy Sablin <anatoliy1994r@yandex.ru>
2025-06-17 17:28:35 +0300

    Чистовая доработка1

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
@@ -53,6 +54,11 @@ public class FilmService {
     }
 
     public Film updateFilm(Film film) {
+        try {
+            filmStorage.getFilmById(film.getId());
+        } catch (NotFoundException e) {
+            throw new NotFoundException("Ошибка! Такой фильм не найден...");
+        }
         filmStorage.updateFilm(film);
         return film;
     }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
@@ -66,6 +67,11 @@ public class UserService {
     }
 
     public User updateUser(User user) {
+        try {
+            userStorage.getUserById(user.getId());
+        } catch (NotFoundException e) {
+            throw new NotFoundException("Пользователь с ID " + user.getId() + " не зарегистрирован");
+        }
         userStorage.updateUser(user);
         return user;
     }
